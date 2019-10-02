a1a88300094c82dcebf5b34801c9933be3148b9f
higumachan <harekumo792154@gmail.com>
2019-10-02 17:28:50 +0900

    write setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+    name='chainer-dataset-utility',
+    version='0.1.0',
+    description='',
+    author='higumachan',
+    author_email='yuta.hinokuma725@gmail.com',
+    license='MIT',
+    url='https://github.com/higumachan/chainer-dataset-utility',
+    packages=["chainerds"],
+)
