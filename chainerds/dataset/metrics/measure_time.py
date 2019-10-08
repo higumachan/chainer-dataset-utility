9613bae925a38ec595656ee4d4266ac388ff9721
higumachan <harekumo792154@gmail.com>
2019-10-08 17:37:57 +0900

    fix

---
+++ chainerds/dataset/metrics/measure_time.py
@@ -0,0 +1,15 @@
+from chainer.dataset import DatasetMixin
+import time
+
+
+class MeasureTime(DatasetMixin):
+    def __init__(self, super_dataset: DatasetMixin):
+        self.super_dataset = super_dataset
+
+    def get_example(self, i):
+        s = time.time()
+        example = self.super_dataset.get_example(i)
+        e = time.time()
+
+        print(e - s)  # TODO(higumachan): ほんとはReporterに飛ばす
+        return example
